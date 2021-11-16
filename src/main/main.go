0724fb59ccdc998dd8b9fb8456e259783a040f0e
rajkrishnamurthy <raj.krishnamurthy@continube.com>
2021-11-16 11:28:53 -0800

    updated main.README.md with resource pattern

--- src/main/main.go
+++ src/main/main.go
@@ -1,10 +1,32 @@
 package main
 
-import libs "commonlibs"
+import (
+	libs "commonlibs"
+	"log"
+)
+
+var controlsVO *libs.ControlsVO
+var metricsVO *libs.MetricsVO
 
 func main() {
-	libs.GenerateSampleData()
+
+	// libs.GenerateSampleData()
 	// libs.LoadFileData()
 	// libs.DataDemo()
+
+}
+
+func init() {
+	controlsVO = new(libs.ControlsVO)
+	controlsFile := "../../configFiles/globalConfig/controls.json"
+	if err := controlsVO.LoadControlsData(controlsFile, "json"); err != nil {
+		log.Printf("%s", err)
+	}
+
+	metricsVO = new(libs.MetricsVO)
+	metricsFile := "../../configFiles/globalConfig/metrics.json"
+	if err := metricsVO.LoadMetricsData(metricsFile, "json"); err != nil {
+		log.Printf("%s", err)
+	}
 
 }
